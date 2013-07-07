15f1bbe49f4aa7e40a8a35b55abb5cdebd103bcc
osktgui <osktgui@gmail.com>
2013-07-07 00:41:23 -0500

    Initial Commit

---
+++ AudioVisual/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AudioVisual
+//
+//  Created by mac developer on 7/7/13.
+//  Copyright (c) 2013 mac developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ AudioVisual/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AudioVisual
+//
+//  Created by mac developer on 7/7/13.
+//  Copyright (c) 2013 mac developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
